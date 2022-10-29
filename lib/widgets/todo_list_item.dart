b058cc8ef506ad9d5679a9bf28f620fa842e6656
Estelson Medeiros Pereira <estelson@gmail.com>
2022-10-29 11:27:21 -0300

    Configurando o componente flutter_slidable para futuramente excluir tarefas
    v. 0.6.0

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
 import '../models/todo.dart';
@@ -10,31 +11,42 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(4),
-      ),
-      margin: const EdgeInsets.symmetric(vertical: 2),
-      padding: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            DateFormat("dd/MM/yyyy - HH:mm:ss").format(todo.dateTime),
-            style: TextStyle(
-              fontSize: 12,
+    return Slidable(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(4),
+        ),
+        margin: const EdgeInsets.symmetric(vertical: 2),
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat("dd/MM/yyyy - HH:mm:ss").format(todo.dateTime),
+              style: TextStyle(
+                fontSize: 12,
+              ),
             ),
-          ),
-          Text(
-            todo.title,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
+            Text(
+              todo.title,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+      actionExtentRatio: 0.25,
+      actionPane: const SlidableStrechActionPane(),
+      secondaryActions: [
+        IconSlideAction(
+          color: Colors.red,
+          icon: Icons.delete,
+          onTap: () {},
+        ),
+      ],
     );
   }
 }
